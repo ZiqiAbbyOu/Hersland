43abaf8db7a4c210cb6f37993b3585a910f36f7e
ZiqiAbbyOu <145293055+ZiqiAbbyOu@users.noreply.github.com>
2024-05-12 18:08:58 -0400

    Property Optimization
    Add description to property

--- Assets/Scripts/Characters/PropertyManager.cs
+++ Assets/Scripts/Characters/PropertyManager.cs
@@ -7,43 +7,14 @@ using UnityEngine;
 namespace HL.Character
 {
 
-    public enum Property
-    {
-        Jin,
-        Mu,
-        Shui,
-        Huo,
-        Tu
-    }
+    
 
     /// <summary>
     /// functions that manage all the character's personlaity
     /// </summary>
     public class PropertyManager : MonoBehaviour
     {
-        public int GetPersonalityArrayInt(Property property)
-        {
-            int propertyArrayInt = 0;
-            switch (property)
-            {
-                case Property.Jin:
-                    propertyArrayInt = 0;
-                    break;
-                case Property.Mu:
-                    propertyArrayInt = 1;
-                    break;
-                case Property.Shui:
-                    propertyArrayInt = 2;
-                    break;
-                case Property.Huo:
-                    propertyArrayInt = 3;
-                    break;
-                case Property.Tu:
-                    propertyArrayInt = 4;
-                    break;
-            }
-            return propertyArrayInt;
-        }
+        
     }
 
 }
