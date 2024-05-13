57fcbfcbc7404e2beb104c9a374c9424b00d2350
ZiqiAbbyOu <145293055+ZiqiAbbyOu@users.noreply.github.com>
2024-05-13 19:02:36 -0400

    Added personality property to the characters
    personality now contains:
    1. stats
    2. descriptions

---
+++ Assets/Scripts/Characters/PersonalityPropertyInfo.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Reflection;
+using UnityEngine;
+
+
+namespace HL.Character
+{
+    public class PersonalityPropertyInfo : WuXingPropertyInfo
+    {
+        
+        protected override void InitializeInfos()
+        {
+            base.InitializeInfos();
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
---
+++ Assets/Scripts/Characters/WuXingPropertyInfo.cs
@@ -0,0 +1,128 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace HL.Character
+{
+    
+    /// <summary>
+    /// Enumeration of properties
+    /// </summary>
+    public enum WuXingProperty
+    {
+        Jin,
+        Mu,
+        Shui,
+        Huo,
+        Tu
+    }
+
+    /// <summary>
+    /// Abstract property class for one single character
+    /// </summary>
+    public abstract class WuXingPropertyInfo : MonoBehaviour
+    {
+        
+
+        [SerializeField] protected float[] wuXingPropertyStats = new float[5];
+        [SerializeField] protected string[] wuXingPropertyDescriptions = new string[5];
+
+
+        protected virtual void Awake()
+        {
+            InitializeInfos();
+        }
+
+        /// <summary>
+        /// Initializes property stats. Can be overridden in derived classes for custom initialization.
+        /// </summary>
+        protected virtual void InitializeInfos()
+        {
+            // Default initialization 
+            for (int i = 0; i < wuXingPropertyStats.Length; i++)
+            {
+                //wuXingPropertyStats[i] = 0f;
+                //wuXingPropertyDescriptions[i] = string.Empty;
+            }
+            
+        }
+
+        /// <summary>
+        /// Get property array int base on property name
+        /// </summary>
+        /// <param name="property">The property type to access</param>
+        /// <returns>The property array int</returns>
+        public int GetPropertyArrayInt(WuXingProperty property)
+        {
+            return (int)property;
+        }
+
+        /// <summary>
+        /// Gets the stat value for the specified property.
+        /// </summary>
+        /// <param name="property">The property type to access.</param>
+        /// <returns>The value of the specified property stat.</returns>
+        public float GetPropertyStat(WuXingProperty property)
+        {
+            int propertyArrayInt = GetPropertyArrayInt(property);
+
+            if (property < 0 || propertyArrayInt >= wuXingPropertyStats.Length)
+            {
+                Debug.LogError("Property index out of range.");
+                return 0f;
+            }
+            return wuXingPropertyStats[propertyArrayInt];
+        }
+
+
+        /// <summary>
+        /// Get the discription for the specified property
+        /// </summary>
+        /// <param name="property">The property type to access</param>
+        /// <returns>The discription for the specified property</returns>
+        public string GetPropertyDescription(WuXingProperty property)
+        {
+            int propertyArrayInt = GetPropertyArrayInt(property);
+
+            if (property < 0 || propertyArrayInt >= wuXingPropertyStats.Length)
+            {
+                Debug.LogError("Property index out of range.");
+                return "";
+            }
+            return wuXingPropertyDescriptions[propertyArrayInt];
+        }
+
+        /// <summary>
+        /// Set the stat value for the specified property
+        /// </summary>
+        /// <param name="property">The property type to access</param>
+        /// <param name="newValue">The new property value</param>
+        public void SetPropertyStat(WuXingProperty property, float newValue)
+        {
+            int propertyArrayInt = GetPropertyArrayInt(property);
+
+            if (property < 0 || propertyArrayInt >= wuXingPropertyStats.Length)
+            {
+                Debug.LogError("Property index out of range.");
+            }
+            wuXingPropertyStats[propertyArrayInt] = newValue;
+        }
+
+        /// <summary>
+        /// Set the description value for the specified property
+        /// </summary>
+        /// <param name="property">The property type to access</param>
+        /// <param name="newDescription">The new property description</param>
+        public void SetPropertyDescription(WuXingProperty property,string newDescription )
+        {
+            int propertyArrayInt = GetPropertyArrayInt(property);
+
+            if (property < 0 || propertyArrayInt >= wuXingPropertyStats.Length)
+            {
+                Debug.LogError("Property index out of range.");
+            }
+            wuXingPropertyDescriptions[propertyArrayInt] = newDescription;
+        }
+    }
+}
