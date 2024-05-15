de0c9a05d97aded9f787ce2c04c1b92ec0469987
ZiqiAbbyOu <145293055+ZiqiAbbyOu@users.noreply.github.com>
2024-05-15 17:41:57 -0400

    Import GOAP package

---
+++ Assets/Scripts/Characters/GOAP/Goals/WanderGoal.cs
@@ -0,0 +1,22 @@
+using CrashKonijn.Goap.Behaviours;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HL.Character.GOAP
+{
+    public class WanderGoal : GoalBase
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
